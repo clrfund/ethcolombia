d8d80de5107fceb42c83b223c164f54c1fe09ec3
yuetloo <contact@yuetloo.com>
2023-01-30 18:38:43 -0500

    ignore error on resolving ens name

--- src/utils/accounts.ts
+++ src/utils/accounts.ts
@@ -2,6 +2,11 @@ import { mainnetProvider } from '@/api/core'
 
 // Looks up possible ENS for given 0x address
 export async function ensLookup(address: string): Promise<string | null> {
-  const name: string | null = await mainnetProvider.lookupAddress(address)
-  return name
+  try {
+    const name: string | null = await mainnetProvider.lookupAddress(address)
+    return name
+  } catch {
+    // in case the url is bad or any error looking up address, return null to not resolve it
+    return null
+  }
 }
